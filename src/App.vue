ff9e782e0b19dca31c6cbd83a2cbdfcfcc0d7a32
Terrance <terrancebryant85@gmail.com>
2021-01-05 15:36:15 -0600

    Initial placeholder

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <h1>Terrance Bryant</h1>
+  <!-- <router-view/> -->
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    * {
+        margin: 0;
+        padding: 0;
     }
-  }
-}
+    h1 {
+        font-family: 'Poppins', sans-serif;
+        text-transform: uppercase;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+    }
 </style>
